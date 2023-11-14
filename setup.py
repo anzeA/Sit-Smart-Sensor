e900d669f0d6ae2fd7328544855d72e9977048c8
Anže Alič <anze.alic@gmail.com>
2023-11-14 19:48:14 +0100

    update requirements and setup

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
-#setup.py file
+# setup.py file
 from setuptools import setup, find_packages
+
 setup(
-    name="sitSmart-Sensor",
+    name="sit_smart_sensor",
     version="0.1",
     packages=find_packages(),
+    package_dir={
+        '': 'src',
+    },
+    requires=[
+        "torch",
+        "torchvision",
+        "lightning"
+        "opencv-python",
+        "hydra-core",
+        "numpy",
+        "tensorboardx",
+        "torchmetrics",
+        "ax-platform",
+        "playsound == 1.2.2",
+        "grad-cam"
 
+    ]
 )
