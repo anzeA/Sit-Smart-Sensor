40c02acbc58ca0daf11d53cf1ba85281c93f169e
Anže Alič <anze.alic@gmail.com>
2023-11-19 12:40:11 +0100

    fix install_requires

--- setup.py
+++ setup.py
@@ -1,24 +1,17 @@
 # setup.py file
 from setuptools import setup, find_packages
 
+from pathlib import Path
+dir = Path(__file__).parent.absolute()
+with open(dir / 'requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name="sit_smart_sensor",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     package_dir={
         '': 'src',
     },
-    install_requires=[
-        "torch",
-        "torchvision",
-        "lightning",
-        "opencv-python",
-        "hydra-core",
-        "numpy",
-        "tensorboardx",
-        "torchmetrics",
-        "ax-platform",
-        "playsound <= 1.2.2",
-        "grad-cam"
-    ]
+    install_requires=required
 )
